1d2d6f60a3698db9e2b8cec0ae4c8514eee9a3d0
AlexMunive <103223036+AlexMunive@users.noreply.github.com>
2022-11-05 16:38:18 -0500

    Posts

---
+++ src/posts/posts.controller.js
@@ -0,0 +1,81 @@
+const Posts = require('../models/posts.models')
+
+const uuid = require('uuid')
+const Users = require('../models/users.models')
+const Categories = require('../models/categories.models')
+
+const getAllPosts = async(offset,limit,name) => {
+    const data = await Posts.findAndCountAll({
+        offset: offset,
+        limit: limit,
+        // name: name? name:name,
+        attributes: {
+            exclude: ['userId', 'categoryId', 'createdAt', 'updatedAt']
+         },
+        include: [
+            {
+                model: Users,
+                as: 'user',
+                attributes: ['id', 'firstName', 'lastName', 'email']
+            },
+            {
+
+                model: Categories
+            }
+        ]
+    })
+    return data
+}
+
+const getPostById = async(id) => {
+    const data = await Posts.findOne({
+        where: {
+            id
+        },
+        attributes: {
+            exclude: ['userId', 'categoryId', 'createdAt', 'updatedAt']
+         },
+        include: [
+            {
+                model: Users,
+                as: 'user',
+                attributes: ['id', 'firstName', 'lastName', 'email']
+            },
+            {
+                model: Categories,
+                as: 'category'
+            }
+        ]
+    })
+    return data
+}
+
+const createPost = async (data) => {
+    const response = await Posts.create({
+        id: uuid.v4(),
+        title: data.title,
+        content: data.content,
+        userId: data.userId, //? este es el user id que viene desde el token
+        categoryId: data.categoryId
+    })
+    return response
+}
+
+const getPostsByCategory = async (categoryId) => {
+    const data = await Posts.findAll({
+        where: {
+            categoryId
+        }
+    })
+    return data
+}
+
+
+
+module.exports = {
+    getAllPosts,
+    getPostById,
+    createPost,
+    getPostsByCategory
+}
+
